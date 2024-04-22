9193499718f151a63a4141adda76a685ad930903
Kettenkrado <111424942+Kettenkrado@users.noreply.github.com>
2024-04-22 16:59:30 +0000

    something

--- projects/hw07/hw07.scm
+++ projects/hw07/hw07.scm
@@ -1,11 +1,18 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+  (if (odd? exp) (* base (pow base (- exp 1)))
+      (if (zero? exp) 1
+          (* (square (pow base (/ exp 2))))
+      )
+  )
+)
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
+      (let 
+        ((y (repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
