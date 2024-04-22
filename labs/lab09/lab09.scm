a20158147a2bae58b51be40a66d12f6fd05981b0
Kettenkrado <111424942+Kettenkrado@users.noreply.github.com>
2024-04-22 16:31:33 +0000

    did lab09

--- labs/lab09/lab09.scm
+++ labs/lab09/lab09.scm
@@ -1,10 +1,36 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+  (if (>= num1 num2)
+      (if (> num1 num2)
+          1
+          0)
+      -1)
+)
+; if version
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+  (cond ((> num1 num2) 1)
+        ((= num1 num2) 0)
+        (else -1))
+)
+; cond version
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  (define (adder inc)
+    (+ num inc))
+  adder
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  (define (compute x)
+    (f (g x)))
+  compute
+)
+
+(define (repeat f n) 
+  (define (repeat-on x)
+    (if (> n 0) ((repeat f (- n 1))(f x)) x))
+  repeat-on
+)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +42,13 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  (if (or (zero? a) (zero? b))
+
+      (if (zero? a) b a)
+
+      (let ((r (modulo (max a b) (min a b))))
+        (gcd r (min a b))
+      )
+  )
+)
