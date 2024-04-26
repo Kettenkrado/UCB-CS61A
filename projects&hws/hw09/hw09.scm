f0361cc7c093225be5d3768f354f936e70587594
Kettenkrado <miaolingtong@yeah.net>
2024-04-26 20:53:52 +0800

    did hw09

--- projects&hws/hw09/hw09.scm
+++ projects&hws/hw09/hw09.scm
@@ -1,13 +1,21 @@
-(define (curry-cook formals body) 'YOUR-CODE-HERE)
+(define (curry-cook formals body) 
+  (if (null? formals)
+      body
+      (list 'lambda (list (car formals))
+        (curry-cook (cdr formals) body)))
+)
 
 (define (curry-consume curry args)
-  'YOUR-CODE-HERE)
+  (if (null? args)
+      curry
+      (curry-consume (curry (car args)) (cdr args)))
+)
 
 (define-macro (switch expr options)
   (switch-to-cond (list 'switch expr options)))
 
 (define (switch-to-cond switch-expr)
-  (cons _________
+  (cons 'cond
         (map (lambda (option)
-               (cons _______________ (cdr option)))
+               (cons (list 'equal? (car (cdr switch-expr)) (car option)) (cdr option)))
              (car (cdr (cdr switch-expr))))))
