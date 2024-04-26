525520234bc7e0e53c1747070497ca9e69350cbe
Kettenkrado <miaolingtong@yeah.net>
2024-04-26 21:12:01 +0800

    did lab11

--- labs/lab11/lab11.scm
+++ labs/lab11/lab11.scm
@@ -1,15 +1,20 @@
 (define (if-program condition if-true if-false)
-  'YOUR-CODE-HERE)
+  `(if ,condition ,if-true ,if-false)
+)
 
 (define (square n) (* n n))
 
-(define (pow-expr base exp) 'YOUR-CODE-HERE)
+(define (pow-expr base exp) 
+  (cond ((zero? exp) 1)
+        ((odd?  exp) `(* ,base ,(pow-expr base (- exp 1))))
+        ((even? exp) `(square ,(pow-expr base (/ exp 2)))))
+)
 
 (define-macro (repeat n expr)
-  `(repeated-call ,n ___))
+  `(repeated-call ,n (lambda () ,expr)))
 
 ; Call zero-argument procedure f n times and return the final result.
 (define (repeated-call n f)
   (if (= n 1)
-      ___
-      (begin ___ ___)))
+      (f)
+      (begin (f) (repeated-call (- n 1) f))))
